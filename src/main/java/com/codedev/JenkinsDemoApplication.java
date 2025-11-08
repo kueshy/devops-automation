1c2a289698d58699d5f781e933f1a0b70e099776
elias <eliasemonyi@yahoo.com>
2025-11-08 13:05:31 +0300

    update jenkins file

--- src/main/java/com/codedev/JenkinsDemoApplication.java
+++ src/main/java/com/codedev/JenkinsDemoApplication.java
@@ -5,17 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Main class for the Jenkins demo application.
+ */
 @SpringBootApplication
 @RestController
 public class JenkinsDemoApplication {
 
+    /**
+     * Test endpoint for Jenkins.
+     *
+     * @return greeting string
+     */
     @GetMapping
     public String testJenkins() {
         return "Hello Jenkins";
     }
 
-    public static void main(String[] args) {
+    /**
+     * Application entry point.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
-
 }
